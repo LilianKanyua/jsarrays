b56314b7b544bd05719e3a4669c2501dba112c32
LilianKanyua <liliankanyua@gmail.com>
2023-03-15 10:31:01 +0300

    first commit

---
+++ arrays.js
@@ -0,0 +1,23 @@
+arr1 =[3,7,34,90,12];
+arr2 =[true,"green","where",12,56];
+
+lastEl1=arr1[arr1.length-1];
+console.log(lastEl1);
+
+console.log(arr2[arr2.length -1]);
+
+myPets=["cow","bird","snake","Dog"];
+console.log(myPets.join(","));
+
+var arr3=[-5,9,5,3,2,-3,6,8,4,1];
+arr3.sort();
+console.log(arr3);
+
+var arr=["apple","mango","apple","orange","mango","mango"];
+
+
+
+let arr5 =["the","way","x",4];
+
+let word="sevink";
+console.log(word.sort);
